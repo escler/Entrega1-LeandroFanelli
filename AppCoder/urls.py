a4612421d161ca5b3b7d2cf5dc0c0add6bee421c
escler <fanelli.leandro.sebastian@gmail.com>
2022-01-21 18:19:38 -0300

    Templates

---
+++ AppCoder/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from AppCoder.views import buscarCamada, busquedaCamada, crear_curso, cursos, inicio, profesores, entregables, estudiantes, cursoFormulario, profesorFormulario
+
+urlpatterns = [
+    path('crear_curso/<camada>', crear_curso),
+    path('', inicio, name='Inicio'),
+    path('cursos', cursos, name='Cursos'),
+    path('profesores', profesores, name='Profesores'),
+    path('entregables', entregables, name='Entregables'),
+    path('estudiantes', estudiantes, name='Estudiantes'),
+    path('cursoFormulario', cursoFormulario, name='CursoFormulario'),
+    path('profesorFormulario', profesorFormulario, name='ProfesorFormulario'),
+    path('busquedaCamada', busquedaCamada, name='BusquedaCamada'),
+    path('buscarCamada/', buscarCamada),
+]
